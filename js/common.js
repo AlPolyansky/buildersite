5e04430915bef69f35995c491a66754a8fd46e4b
Eclerr <eclerrrr@yandex.ru>
2016-03-12 06:26:06 +0300

    added parallax

--- js/common.js
+++ js/common.js
@@ -1,18 +1,25 @@
 $(document).ready(function() {
 	var content = $("#content");
 
+
+
+
 	function contentHeight(){
-		content.find(".content_about").height($(window).height());
+		$(".content_about").css("height", $(window).height());
 	}
 
 	
 	contentHeight();
 
-	
+	//$(".content_about").stellar();
+
+
+
+
 	$(window).resize(function(){
 		contentHeight();
 	})
 
-
+	//$('.content_about').parallax({imageSrc: 'img/room_1.jpg'});
 
 })
